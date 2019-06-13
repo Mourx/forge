317cdc44ecad47705ff527e8888c8f370b2d921a
Joel <psyjc5@nottingham.ac.uk>
2019-06-13 14:53:25 +0100

    Added base content

---
+++ forge-game/src/main/java/forge/game/spellability/OptionalCost.java
@@ -0,0 +1,31 @@
+package forge.game.spellability;
+
+/** 
+ * TODO: Write javadoc for this type.
+ *
+ */
+public enum OptionalCost {
+    Conspire("Conspire"),
+    Buyback("Buyback"),
+    Entwine("Entwine"),
+    Kicker1("Kicker"),
+    Kicker2("Kicker"),
+    Retrace("Retrace"),
+    Jumpstart("Jump-start"),
+    AltCost(""),
+    Flash("Flash"), // used for Pay Extra for Flash
+    Generic("Generic"); // used by "Dragon Presence" and pseudo-kicker cards
+
+    private String name;
+    
+    OptionalCost(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+}
