06155c11bdead4c29d02042bbc864c4b044eb1ea
Joel <psyjc5@nottingham.ac.uk>
2019-12-01 00:49:53 +0000

    +rep

---
+++ Forge/forge-ai/src/main/java/forge/ai/simulation/CardTypeSorter.java
@@ -0,0 +1,43 @@
+package forge.ai.simulation;
+import forge.game.ability.*;
+import forge.game.card.Card;
+
+import java.util.ArrayList;
+
+import forge.card.*;
+
+public class CardTypeSorter {
+	
+	
+	public ArrayList<Integer> GetTypes(Card c){
+		ArrayList<Integer> types = new ArrayList<Integer>();
+		
+		CardTypeView v = c.getType();
+
+		int i = v.isArtifact() ? 1 : 0;
+		types.add(i);
+		i = v.isBasicLand() ? 1:0;
+		types.add(i);
+		i = v.isBasicLand() ? 1:0;
+		types.add(i);
+		i = v.isCreature() ? 1:0;
+		types.add(i);
+		i = v.isEnchantment() ? 1:0;
+		types.add(i);
+		i = v.isInstant() ? 1:0;
+		types.add(i);
+		i = v.isLand() ? 1:0;
+		types.add(i);
+		i = v.isLegendary() ? 1:0;
+		types.add(i);
+		i = v.isPermanent() ? 1:0;
+		types.add(i);
+		i = v.isPlaneswalker() ? 1:0;
+		types.add(i);
+		i = v.isSorcery() ? 1:0;
+		types.add(i);
+		
+		
+		return types;
+	}
+}
