3d1cab724f9b2a2510cc0247a28073283d0ca5fe
Joel <psyjc5@nottingham.ac.uk>
2020-01-28 23:41:14 +0000

    CardSorter Needs Creature Amounts

--- Forge/forge-ai/src/main/java/forge/ai/simulation/CardTypeSorter.java
+++ Forge/forge-ai/src/main/java/forge/ai/simulation/CardTypeSorter.java
@@ -1,6 +1,8 @@
 package forge.ai.simulation;
 import forge.game.ability.*;
 import forge.game.card.Card;
+import forge.game.spellability.SpellAbility;
+import forge.util.collect.FCollectionView;
 
 import java.util.ArrayList;
 
@@ -13,7 +15,9 @@ public class CardTypeSorter {
 		ArrayList<Integer> types = new ArrayList<Integer>();
 		
 		CardTypeView v = c.getType();
-
+		
+		//Add Type Information
+		//Ignoring weird types like conspiracy, tribal, etc.
 		int i = v.isArtifact() ? 1 : 0;
 		types.add(i);
 		i = v.isBasicLand() ? 1:0;
@@ -36,9 +40,55 @@ public class CardTypeSorter {
 		types.add(i);
 		i = v.isSorcery() ? 1:0;
 		types.add(i);
-		//comment
 		
+		//Add a mark to show creature has ability, 1 for mana, another 1 for 
+		// non mana-ability
+		FCollectionView<SpellAbility> fv = c.getAllSpellAbilities();
+		int j = 0, k =0, pow = 0, tou = 0;
+		i = 0;
+		//if creature we evaluate if has activatable abilities
+		if(v.isCreature()) {
+			if(fv.size()!=0) {
+				for(SpellAbility sp:fv) {
+					i = (sp.isManaAbility() && i== 0) ? 1:0;
+					j = (sp.isManaAbility() && j== 0) ? 0:1;
+					k = (c.hasETBTrigger(false) && k==0) ? 1:0;
+				}
+			}
+			pow = c.getBasePower();
+			tou = c.getBaseToughness();
+		}
+		//mana ability
+		types.add(i);
+		//other ability
+		types.add(j);
+		//ETB effect;
+		types.add(k);
+		//power
+		types.add(pow);
+		//toughness
+		types.add(tou);
 		
+		//CMC
+		int cmc = c.getCMC();
+		types.add(cmc);
+		
+		//Actual Mana Cost
+		int[] wubrg = c.getManaCost().getColorShardCounts();
+		//White
+		types.add(wubrg[0]);
+		//blUe
+		types.add(wubrg[1]);
+		//Black
+		types.add(wubrg[2]);
+		//Red
+		types.add(wubrg[3]);
+		//Green
+		types.add(wubrg[4]);
+		//Generic mana
+		int generic = c.getManaCost().getGenericCost();
+		types.add(generic);
+				
 		return types;
 	}
 }
