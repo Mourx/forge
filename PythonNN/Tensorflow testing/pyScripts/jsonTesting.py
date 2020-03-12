74e7e0c293dc4f42803b987b52324e631a01ecaf
Joel <psyjc5@nottingham.ac.uk>
2020-03-12 00:00:36 +0000

    json

--- PythonNN/Tensorflow testing/pyScripts/jsonTesting.py
+++ PythonNN/Tensorflow testing/pyScripts/jsonTesting.py
@@ -19,12 +19,17 @@ print(data[0]['name'])
 from keras.preprocessing.text import text_to_word_sequence
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.text import hashing_trick
-name = data[0]['name']
-words = set(text_to_word_sequence(name))
-vocab_size = len(words)
-print(vocab_size,words)
-result = one_hot(name, round(vocab_size*1.3))
-print(result)
 
-result = hashing_trick(name, round(vocab_size*6), hash_function='md5')
-print(result)
+for line in data:
+    name = line['name']
+    print(name)
+    words = set(text_to_word_sequence(name))
+    vocab_size = len(words)
+    print(vocab_size,words)
+    result = one_hot(name, round(vocab_size*1.3))
+    print(result)
+
+    result = hashing_trick(name, round(vocab_size*1.3), hash_function='md5')
+    print(result)
+
+#input type: [nameValue,manaCost,cmc,Type,Color,Power,Toughness,keywordsBin]
